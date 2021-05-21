4b8f923db5718c4d1c64342ad8ab45d935e2359a
Gabriel Olaleye <olaleyegabriel@gmail.com>
2021-05-21 12:51:54 +0100

    Update application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ This script runs the FlaskTemplate application using a development server.
 """
 
 from os import environ
-from FlaskTemplate import app
+from FlakTemplate import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
